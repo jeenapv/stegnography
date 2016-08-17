4d59ec9e6c980fc043a486f87254eca75244ca10
krishnakumarvs <krishh_mea@yahoo.in>
2016-08-17 22:47:53 +0530

    Configuration file added.

---
+++ src/General/Configuration.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package General;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author kakes
+ */
+public class Configuration {
+
+    public static void setIconOnLabel(String file, JLabel label) {
+        // convert string file path to image icona and set on this label
+    }
+}
