0fa4004dbf11d47aee17220f2be083e95831ffc7
krishnakumarvs <krishh_mea@yahoo.in>
2016-08-20 08:27:54 +0530

    Some validation added in EmbedMessage.

--- src/View/EmbedMessage.java
+++ src/View/EmbedMessage.java
@@ -34,8 +34,8 @@ public class EmbedMessage extends javax.swing.JFrame {
         initComponents();
         this.setLocationRelativeTo(null);
         loadIcons();
-       
-        
+        proceed_button.setEnabled(false);
+        analyze_button.setEnabled(false);
     }
       private void loadIcons() {
         Configuration.setIconOnLabel("msg.png", image_Label);
@@ -53,12 +53,12 @@ public class EmbedMessage extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         image_Label = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
+        analyze_button = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
         image_name = new javax.swing.JLabel();
-        jButton4 = new javax.swing.JButton();
+        proceed_button = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -71,13 +71,12 @@ public class EmbedMessage extends javax.swing.JFrame {
             }
         });
 
-        image_Label.setIcon(new javax.swing.ImageIcon("C:\\Users\\Jithinpv\\Documents\\NetBeansProjects\\stegnography\\images\\msg.png")); // NOI18N
         image_Label.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
-        jButton2.setText("Analyse master file");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        analyze_button.setText("Analyse master file");
+        analyze_button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                analyze_buttonActionPerformed(evt);
             }
         });
 
@@ -92,10 +91,10 @@ public class EmbedMessage extends javax.swing.JFrame {
             }
         });
 
-        jButton4.setText("PROCEED");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
+        proceed_button.setText("PROCEED");
+        proceed_button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                proceed_buttonActionPerformed(evt);
             }
         });
 
@@ -112,23 +111,20 @@ public class EmbedMessage extends javax.swing.JFrame {
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(jButton1))
                         .addGroup(layout.createSequentialGroup()
+                            .addGap(57, 57, 57)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                                 .addGroup(layout.createSequentialGroup()
-                                    .addGap(166, 166, 166)
                                     .addComponent(jLabel4)
                                     .addGap(20, 20, 20))
-                                .addGroup(layout.createSequentialGroup()
-                                    .addGap(49, 49, 49)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                        .addComponent(image_name, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(jButton2))))
+                                .addComponent(image_name, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(analyze_button))
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                             .addGap(104, 104, 104)
                             .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGap(18, 18, 18)
-                            .addComponent(jButton4)
+                            .addComponent(proceed_button)
                             .addGap(87, 87, 87)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(117, 117, 117)
@@ -150,11 +146,11 @@ public class EmbedMessage extends javax.swing.JFrame {
                 .addComponent(image_Label, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
+                    .addComponent(analyze_button)
                     .addComponent(jLabel3))
                 .addGap(38, 38, 38)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton4)
+                    .addComponent(proceed_button)
                     .addComponent(jButton3))
                 .addContainerGap(65, Short.MAX_VALUE))
         );
@@ -162,12 +158,13 @@ public class EmbedMessage extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void analyze_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_analyze_buttonActionPerformed
         // TODO add your handling code here:
         store_analyse_masterfile();
+        proceed_button.setEnabled(true);
         
 
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_analyze_buttonActionPerformed
 
     public void store_analyse_masterfile(){
         Dbcon dbcon=new Dbcon();
@@ -220,7 +217,7 @@ public class EmbedMessage extends javax.swing.JFrame {
                        System.out.println(rs.getString(1));
                        EmbedMessage.process_id=Integer.parseInt(rs.getString(1));
                        System.out.println(EmbedMessage.process_id);
-                       
+                        analyze_button.setEnabled(true);
                     } 
                 } catch (SQLException ex) {
                     ex.printStackTrace();
@@ -231,12 +228,12 @@ public class EmbedMessage extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+    private void proceed_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_proceed_buttonActionPerformed
         // TODO add your handling code here:
         this.dispose();
         MessageEncryption messageEncryption = new MessageEncryption();
         messageEncryption.setVisible(true);
-    }//GEN-LAST:event_jButton4ActionPerformed
+    }//GEN-LAST:event_proceed_buttonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -274,14 +271,14 @@ public class EmbedMessage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton analyze_button;
     private javax.swing.JLabel image_Label;
     private javax.swing.JLabel image_name;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JButton proceed_button;
     // End of variables declaration//GEN-END:variables
 }
